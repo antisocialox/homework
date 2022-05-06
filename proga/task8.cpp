3b49a3beb47bdfad38f88cf5ae59ab4f58d95ebc
Andrey <vavilovo64@gmail.com>
2022-05-06 19:27:18 +0400

    Добавил первое задание

--- proga/task8.cpp
+++ proga/task8.cpp
@@ -13,3 +13,40 @@ struct queue{
     queue* next;
 };
 
+// ввод графа
+void input(vector <vector <int>>& gr) {
+    int x, y;
+    for (int i = 0; i < gr.size(); ++i) {
+        cin >> x >> y;
+        gr[x].push_back(y);
+    }
+}
+
+// Дан ориентированный граф. Вывести на экран все вершины, не смежные с данной.
+void task1() {
+    int n, X; // Х - данная вершина
+    cin >> n;
+    vector <vector <int>> gr(n);
+    input(gr);
+    cin >> X;
+    vector <bool> vert(n, true); // вершины, не смежные с данной - true
+    // убираю вершины, следующие из Х
+    for (int i = 0; i < gr[X].size(); ++i) {
+        vert[gr[X][i]] = false;
+    }
+    // убираю вершины, из которых следует Х
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < gr[i].size(); ++j) {
+            if (gr[i][j] == X) {
+                vert[i] = false;
+                break;
+            }
+        }
+    }
+    // вывод
+    for (int i = 0; i < n; ++i) {
+        if (vert[i])
+            cout << i << " ";
+    }
+    cout << endl;
+}
